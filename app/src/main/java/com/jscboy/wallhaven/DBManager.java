6688c29ac99c9b1a1f560e40ef4c46046275268f
JasdipC <jasdip.chauhan@gmail.com>
2016-07-09 00:31:19 -0400

    db logic is fully functional now

--- app/src/main/java/com/jscboy/wallhaven/DBManager.java
+++ app/src/main/java/com/jscboy/wallhaven/DBManager.java
@@ -1,16 +1,18 @@
 package com.jscboy.wallhaven;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBManager extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "wallpapers.db";
-    public static final String TABLE_PROPERTIES = "properties";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_WALLPAPERURL = "wallpaperURL";
+    private static final int DATABASE_VERSION = 1; //increment for version control
+    private static final String DATABASE_NAME = "wallpapers.db"; //file name stored locally on device
+    public static final String TABLE_PROPERTIES = "properties"; //table name
+    public static final String COLUMN_ID = "_id"; //column #1
+    public static final String COLUMN_WALLPAPERURL = "wallpaperURL"; //column #2
 
     public DBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
@@ -29,6 +31,47 @@ public class DBManager extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_PROPERTIES);
-        onCreate(db);
+        onCreate(db); // execute new table code when table is upgraded
     }
+
+    //add wallpaper to db
+    public void addWallpaper(WallpaperProperties wallpaper) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_WALLPAPERURL, wallpaper.get_wallpaperURL());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_PROPERTIES, null, values);
+        db.close();
+    }
+
+    //delete wallpaper from db
+    public void deleteWallpaper(String wallpaperURl) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_PROPERTIES + " WHERE " + COLUMN_WALLPAPERURL + "=\""
+                + wallpaperURl + "\";");
+    }
+
+    //helper function to output db results
+    public String dbToString() {
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_PROPERTIES + " WHERE 1";
+
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+
+        while (!cursor.isAfterLast()) {
+
+            String cursorString = cursor.getString(cursor.getColumnIndex("wallpaperURL"));
+
+            if (cursorString != null) {
+                dbString += cursorString;
+                dbString += "\n";
+            }
+        }
+
+        close();
+
+        return dbString;
+    }
+
 }
